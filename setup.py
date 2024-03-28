9dbc5a23cfda5ed9d956c20dc3824145450e6b0f
DradjatKevin <kevindr@DESKTOP-Q9IC5HE>
2024-03-28 13:40:22 +0100

    merge setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="SSRL_RNAseq",
+    description="Self-supervised methods on gene expression data",
+    author="Kevin Dradjat",
+    url="https://github.com/kdradjat/SSRL_RNAseq",
+    keywords=[
+        "artificial intelligence",
+        "contrastive learning",
+        "self-supervised learning",
+    ],
+    python_requires=">=3.7",
+    install_requires=["torch==1.12", 
+                      "tqdm==4.64", 
+                      'accelerate', 
+                      'beartype',
+                      'torch>=1.6',
+                      'torchvision>=0.8',
+                      'numpy>=1.18.2',
+                      'pandas>=1.0.3',
+                      'keras>=2.3.1',
+                      'argparse>=1.1',
+                      'sklearn>=0.21.3',
+                      'xgboost>=0.90',
+                      'tensorflow==1.15.0'
+                      ]
+)
