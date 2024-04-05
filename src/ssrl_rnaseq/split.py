2906535d2a2b0c4a4208426473824aabea757d75
Pierre Bartet <pierre@adlin-science.com>
2024-04-05 12:05:44 +0200

    WIP split.py

--- src/ssrl_rnaseq/split.py
+++ src/ssrl_rnaseq/split.py
@@ -1,37 +1,69 @@
+from itertools import chain
+
 import numpy as np
 from sklearn.utils import check_random_state, shuffle
 from sklearn.model_selection import StratifiedGroupKFold, train_test_split
 
-__all__ = ["GroupKShotsFold"]
+__all__ = ["pretrain_downstream_split", "GroupKShotsFold"]
 
 
-def pretext_downstream_split(
-        *array,
-        pretext_size=None,
+def pretrain_downstream_split(
+        *arrays,
+        pretrain_size=None,
         downstream_size=None,
         groups,
         stratify=None,
         random_state=None,
 ):
-    unique_groups = np.unique(groups)
+    """
+    Split given `arrays` into pretrain and downstream sets.
+    The pretrain set contains `pretrain_size` groups.
+    The pretrain set contains `downstream_size` groups.
+    Notice that the sizes are not numbers of samples but number of groups.
+    """
 
-    pretext_groups, downstream_groups = train_test_split(
-        unique_groups,
-        train_size=pretext_size,
-        downstream_size=downstream_size,
+    unique_groups, groups = np.unique(groups, return_inverse=True)
+    unique_groups = len(unique_groups)
+
+    # Attribute the group's most common class to each group
+    if stratify is not None:
+        stratify = np.asarray(stratify)
+
+        unique_classes, stratify = np.unique(stratify, return_inverse=True)
+        unique_classes = len(unique_classes)
+
+        s = np.zeros((unique_groups, unique_classes), dtype=np.int64)
+        np.add.at(s, (groups, stratify), 1)
+
+        stratify = s.argmax(axis=1)
+
+    # Split
+    pretrain_groups, downstream_groups = train_test_split(
+        np.arange(unique_groups),
+        train_size=pretrain_size,
+        test_size=downstream_size,
         shuffle=True,
         stratify=stratify,
         random_state=random_state,
     )
 
-    pretext = groups.isin(pretext_groups)
-    downstream = groups.isin(downstream_groups)
+    pretrain = np.isin(groups, pretrain_groups)
+    downstream = np.isin(groups, downstream_groups)
 
-    return pretext
+    return list(
+        chain.from_iterable(
+            (a[pretrain], a[downstream]) for a in arrays
+        )
+    )
 
 
 class GroupKShotsFold:
     def __init__(self, n_splits, *, k, random_state=None):
+        """
+        Same as a StratifiedGroupKFold but only `k` samples per classes are kept in each training
+        split.
+        """
+
         self.n_splits = n_splits
         self.k = k
         self.random_state = random_state
