91984ff4f0df37048cd6f0b66af1bbef460baf51
Kevin DRADJAT <kevin.dradjat@univ-evry.fr>
2024-04-05 11:39:51 +0200

    setup

--- setup.py
+++ setup.py
@@ -11,18 +11,17 @@ setup(
         "self-supervised learning",
     ],
     python_requires=">=3.7",
-    install_requires=["torch==1.12", 
-                      "tqdm==4.64", 
+    install_requires=["torch>=1.12", 
+                      "tqdm>=4.64", 
                       'accelerate', 
                       'beartype',
-                      'torch>=1.6',
-                      'torchvision>=0.8',
+                      'torchvision',
                       'numpy>=1.18.2',
                       'pandas>=1.0.3',
                       'keras>=2.3.1',
                       'argparse>=1.1',
                       'sklearn>=0.21.3',
-                      'xgboost>=0.90',
-                      'tensorflow==1.15.0'
+                      'xgboost',
+                      'tensorflow>=1.15.0'
                       ]
 )
