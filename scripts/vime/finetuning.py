015eb2cdafb7f1b00f572e293c257ed76448351f
Kevin Dradjat <kevindradjat@core-login2.cluster.france-bioinformatique.fr>
2024-05-07 16:16:53 +0200

    vime scripts

--- scripts/vime/finetuning.py
+++ scripts/vime/finetuning.py
@@ -16,51 +16,107 @@ from ssrl_rnaseq.vime.unfreeze import wrapper, wrapper_rework
 
 from keras.models import load_model
 
-
-# load data
-print('Loading Data...')
-dataset = read_process_data('../data/TCGA/nopretrain_data.parquet', '../data/TCGA/label.parquet')
-#dataset = read_process_data('../data/TCGA/100Best_nopretrain_data.parquet.gzip', '../data/TCGA/label.parquet')
-
-# Finetuning
-from scipy.special import softmax
-
-prop_list = np.arange(0.02, 1.0, 0.01)
-csv_history = pd.DataFrame(columns=['test_acc', 'prop'])
-
-# MLP
-mlp_parameters = dict()
-mlp_parameters['hidden_dim'] = 100
-mlp_parameters['epochs'] = 100
-mlp_parameters['activation'] = 'relu'
-mlp_parameters['batch_size'] = 8
-
-metric = 'acc'
-
-results_pretraining = []
-
-for prop in prop_list :
-    for i in range(5) :
-        print(prop)
-        # split data
-        idx = generate_indices(dataset, prop=prop, val_prop = 0.01, test_prop = 0.5, rs=42)
-        dataset_train = dataset[idx[0]]
-        dataset_test = dataset[idx[2]]
-        x_train, y_train = dataset_train[:,1:], dataset_train[:,0]
-        x_test, y_test = dataset_test[:,1:], dataset_test[:,0]
-
-        # load model
-        vime_self_encoder = load_model('./saved_models/vime_4layers.h5')
-
-        # finetune model with mlp head
-        y_test_hat = wrapper_rework(vime_self_encoder, x_train, y_train, x_test, mlp_parameters)
-        results_pretraining.append(perf_metric(metric, y_test, y_test_hat))
-
-        # save 
-        csv_history.loc[len(csv_history)] = [results_pretraining[-1], prop]
-    csv_history.to_csv('unfreeze_vime_4layers.csv')
+import argparse
 
 
+def main():
+    parser = argparse.ArgumentParser()
+    
+    # Required parameters
+    parser.add_argument(
+        "--data_file",
+        default=None,
+        type=str,
+        required=True,
+        help="The finetuning data file. Must be in .parquet format."
+    )
+    parser.add_argument(
+        "--label_file",
+        default=None,
+        type=str,
+        required=True,
+        help="The finetuning label file. Must be in .parquet format."
+    )
+    parser.add_argument(
+        "--batch_size",
+        default=8,
+        type=int,
+        help="batch size"
+    )
+    parser.add_argument(
+        "--epoch",
+        default=100,
+        type=int,
+        help="number of epoch"
+    )
+    parser.add_argument(
+        "--activation",
+        defalut="relu",
+        type=str,
+        help="activation function type"
+    )
+    parser.add_argument(
+        "--model_name",
+        default=None,
+        required=True,
+        type=str,
+        help="name of the pretrained model"
+    )
+    parser.add_argument(
+        "--history_name",
+        default=None,
+        required=True,
+        type=str,
+        help="name of the accuracy history file"
+    )
+    
+    args = parser.parse_args()
+
+    
+    
+    # load data
+    print('Loading Data...')
+    dataset = read_process_data_TCGA(args.data_file, args.label_file)
+
+    # Finetuning
+    from scipy.special import softmax
+
+    prop_list = np.arange(0.02, 1.0, 0.01)
+    csv_history = pd.DataFrame(columns=['test_acc', 'prop'])
+
+    # MLP
+    mlp_parameters = dict()
+    mlp_parameters['epochs'] = args.epoch
+    mlp_parameters['activation'] = args.activation
+    mlp_parameters['batch_size'] = args.batch_size
+
+    metric = 'acc'
+
+    results_pretraining = []
+
+    for prop in prop_list :
+        for i in range(5) :
+            print(prop)
+            # split data
+            idx = generate_indices(dataset, prop=prop, val_prop = 0.01, test_prop = 0.5, rs=42)
+            dataset_train = dataset[idx[0]]
+            dataset_test = dataset[idx[2]]
+            x_train, y_train = dataset_train[:,1:], dataset_train[:,0]
+            x_test, y_test = dataset_test[:,1:], dataset_test[:,0]
+
+            # load model
+            vime_self_encoder = load_model(f'./saved_models/{args.model_name}')
+
+            # finetune model with mlp head
+            y_test_hat = wrapper_rework(vime_self_encoder, x_train, y_train, x_test, mlp_parameters)
+            results_pretraining.append(perf_metric(metric, y_test, y_test_hat))
+
+            # save 
+            csv_history.loc[len(csv_history)] = [results_pretraining[-1], prop]
+        csv_history.to_csv(args.history_name)
 
 
+
+if __name__ == "__main__" :
+    main()
 
--- scripts/vime/pretraining.py
+++ scripts/vime/pretraining.py
@@ -12,50 +12,104 @@ from ssrl_rnaseq.vime.vime_utils import perf_metric
 
 import keras as keras
 
-# Load Data
-print('Loading Data...')
-x_unlabel = read_process_data_TCGA('../data/TCGA/pretrain_data.parquet', '../data/TCGA/label.parquet')
-#x_unlabel = read_process_data_ARCHS4('../data/ARCHS4/specific/pretrain_specific_data.parquet.gzip', '../data/ARCHS4/specific/pretrain_specific_metadata.parquet.gzip')
-#x_unlabel = read_process_data('../data/TCGA/100Best_pretrain_data.parquet.gzip', '../data/TCGA/label.parquet')
-y_unlabel = x_unlabel[:,0]
-x_unlabel = x_unlabel[:,1:]
+import argparse
 
 
 
-# Define Hyper-parameters
-p_m = 0.3 # mask proportion : default 0.3
-alpha = 2.0
-
-# Metric
-metric = 'acc'
-
-# pre-training
-vime_self_parameters = {}
-vime_self_parameters['batch_size'] = 16
-vime_self_parameters['epochs'] = 1000
-#vime_self_encoder, history_mask = vime_self_baseline(x_unlabel, p_m, alpha, vime_self_parameters)
-vime_self_encoder, history_mask = vime_self_4layers(x_unlabel, p_m, alpha, vime_self_parameters)
-#vime_self_encoder, history_mask = vime_self_subtab(x_unlabel, p_m, alpha, vime_self_parameters)
-print(history_mask.history.keys())
-
-if not os.path.exists('saved_models') :
-    os.makedirs('saved_models')
+def main():
+    parser = argparse.ArgumentParser()
     
-file_name = './saved_models/vime_4layers.h5'
+    # Required parameters
+    parser.add_argument(
+        "--data_file",
+        default=None,
+        type=str,
+        required=True,
+        help="The pretraining data file. Must be in .parquet format."
+    )
+    parser.add_argument(
+        "--batch_size",
+        default=16,
+        type=int,
+        help="batch size"
+    )
+    parser.add_argument(
+        "--epoch",
+        default=1000,
+        type=int,
+        help="number of epoch"
+    )
+    parser.add_argument(
+        "--model_name",
+        default="vime.h5",
+        type=str,
+        help="name given to the pretrained model"
+    )
+    parser.add_argument(
+        "--history_name",
+        default="history_vime.csv",
+        type=str,
+        help="name of the loss history file"
+    )
+    parser.add_argument(
+        "--mask_prop",
+        type=float,
+        default=0.3,
+        help="mask proportion"
+    )
+    parser.add_argument(
+        "--alpha",
+        type=float,
+        default=2.0,
+        help="alpha parameters"
+    )
+    
+    args = parser.parse_args()
 
-vime_self_encoder.save(file_name)
+
+    
+    # Load Data
+    print('Loading Data...')
+    x_unlabel = read_process_data_TCGA_unlabel(args.data_file)
+    #x_unlabel = read_process_data_ARCHS4(args.data_file, args.label_file)
+
+    # Define Hyper-parameters
+    p_m = args.mask_prop # mask proportion : default 0.3
+    alpha = args.alpha
+
+    # Metric
+    metric = 'acc'
+
+    # pre-training
+    vime_self_parameters = {}
+    vime_self_parameters['batch_size'] = args.batch_size
+    vime_self_parameters['epochs'] = args.epoch
+    #vime_self_encoder, history_mask = vime_self_baseline(x_unlabel, p_m, alpha, vime_self_parameters)
+    vime_self_encoder, history_mask = vime_self_4layers(x_unlabel, p_m, alpha, vime_self_parameters)
+    #vime_self_encoder, history_mask = vime_self_subtab(x_unlabel, p_m, alpha, vime_self_parameters)
+
+    if not os.path.exists('saved_models') :
+        os.makedirs('saved_models')
+    
+    file_name = f'./saved_models/{args.model_name}'
+
+    vime_self_encoder.save(file_name)
 
 
-# save history as csv
-df = pd.DataFrame(columns=['epoch', 'loss', 'mask_loss', 'feature_loss', 'val_loss', 'val_mask_loss', 'val_feature_loss'])
-df['epoch'] = [i for i in range(vime_self_parameters['epochs'])]
-df['loss'] = history_mask.history['loss']
-df['mask_loss'] = history_mask.history['mask_loss']
-df['feature_loss'] = history_mask.history['feature_loss']
-df['val_loss'] = history_mask.history['val_loss']
-df['val_mask_loss'] = history_mask.history['val_mask_loss']
-df['val_feature_loss'] = history_mask.history['val_feature_loss']
-df.to_csv('history_pretraining_vime_4layers.csv')
+    # save history as csv
+    df = pd.DataFrame(columns=['epoch', 'loss', 'mask_loss', 'feature_loss', 'val_loss', 'val_mask_loss', 'val_feature_loss'])
+    df['epoch'] = [i for i in range(vime_self_parameters['epochs'])]
+    df['loss'] = history_mask.history['loss']
+    df['mask_loss'] = history_mask.history['mask_loss']
+    df['feature_loss'] = history_mask.history['feature_loss']
+    df['val_loss'] = history_mask.history['val_loss']
+    df['val_mask_loss'] = history_mask.history['val_mask_loss']
+    df['val_feature_loss'] = history_mask.history['val_feature_loss']
+    df.to_csv(args.history_name)
+    
+
+if __name__ == '__main__' :
+    main()
 
 
 
