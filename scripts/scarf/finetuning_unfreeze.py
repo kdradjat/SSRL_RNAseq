a7b5c1bbfe79c8a1d94e1ea58902febc0eab58b6
Kevin Dradjat <kevindradjat@core-login1.cluster.france-bioinformatique.fr>
2024-05-13 12:42:33 +0200

    update scripts

--- scripts/scarf/finetuning_unfreeze.py
+++ scripts/scarf/finetuning_unfreeze.py
@@ -18,82 +18,146 @@ from ssrl_rnaseq.scarf.scarf_utils import *
 from ssrl_rnaseq.data import *
 from ssrl_rnaseq.training_utils import *
 
+import argparse
+
 ##############
 
+def main():
+    parser = argparse.ArgumentParser()
+    
+    # Required parameters
+    parser.add_argument(
+        "--pretraining_file",
+        default=None,
+        type=str,
+        required=True,
+        help="The pretraining data file. Must be in .parquet format."
+    )
+    parser.add_argument(
+        "--finetuning_file",
+        default=None,
+        type=str,
+        required=True,
+        help="The finetuning data file. Must be in .parquet format."
+    )
+    parser.add_argument(
+        "--label_file",
+        default=None,
+        type=str,
+        required=True,
+        help="The label data file. Must be in .parquet format."
+    )
+    parser.add_argument(
+        "--batch_size",
+        default=8,
+        type=int,
+        help="batch size"
+    )
+    parser.add_argument(
+        "--corruption_rate",
+        default=0.3,
+        type=float,
+        help="corruption rate"
+    )
+    parser.add_argument(
+        "--dropout",
+        default=0.1,
+        type=float,
+        help="dropout rate"
+    )
+    parser.add_argument(
+        "--model_name",
+        default=None,
+        required=True,
+        type=str,
+        help="name of the pretrained model"
+    )
+    parser.add_argument(
+        "--history_name",
+        default=None,
+        required=True,
+        type=str,
+        help="name of the accuracy history file"
+    )
+    parser.add_argument(
+        "--epoch",
+        defalut=100,
+        type=int,
+        help="number of epoch"
+    )
+    args = parser.parse_args()
 
-# Load Data
-print('Loading Data...')
-dataset_pretrain = read_process_data_TCGA('../data/TCGA/pretrain_data.parquet', '../data/TCGA/label.parquet')
-dataset = read_process_data_TCGA('../data/TCGA/nopretrain_data.parquet', '../data/TCGA/label.parquet')
-#dataset_pretrain = read_process_data_ARCHS4('../data/ARCHS4/specific/pretrain_specific_data.parquet.gzip', '../data/ARCHS4/specific/pretrain_specific_metadata.parquet.gzip')
-#dataset = read_process_data_ARCHS4('../data/ARCHS4/specific/nopretrain_specific_data.parquet.gzip', '../data/ARCHS4/specific/nopretrain_specific_metadata.parquet.gzip')
-nb_classes = len(np.unique(dataset[:,0]))
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # Load Data
+    print('Loading Data...')
+    dataset_pretrain = read_process_data_TCGA(args.pretraining_file, args.label_file)
+    dataset = read_process_data_TCGA(args.finetuning_file, args.label_file)
+    #dataset_pretrain = read_process_data_ARCHS4('../data/ARCHS4/specific/pretrain_specific_data.parquet.gzip', '../data/ARCHS4/specific/pretrain_specific_metadata.parquet.gzip')
+    #dataset = read_process_data_ARCHS4('../data/ARCHS4/specific/nopretrain_specific_data.parquet.gzip', '../data/ARCHS4/specific/nopretrain_specific_metadata.parquet.gzip')
+    nb_classes = len(np.unique(dataset[:,0]))
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-pretrain_ds = ExampleDataset(
-    dataset_pretrain[:,1:],
-    dataset_pretrain[:,0]
-)
+    pretrain_ds = ExampleDataset(
+        dataset_pretrain[:,1:],
+        dataset_pretrain[:,0]
+    )
 
-# Training
-name = "scarf_unfreeze_4layers"
-logger = LogResults(name, ["prop"])
-bs = 8
-prop_list = np.arange(0.02, 1.0, 0.01)
-corruption_rate = 0.5
-dropout = 0.1
+    # Training
+    name = f"{args.history_name}"
+    logger = LogResults(name, ["prop"])
+    bs = args.batch_size
+    prop_list = np.arange(0.02, 1.0, 0.01)
+    corruption_rate = args.corruption_rate
+    dropout = args.dropout
 
 
-for i in range(3) :
-    for prop in prop_list :
-        # logger
-        logger.update_hyps([prop])
+    for i in range(3) :
+        for prop in prop_list :
+            # logger
+            logger.update_hyps([prop])
         
-        # Build and Load model
-        # build model
-        pretrained_model = SCARF_4layers(
-            input_dim=pretrain_ds.shape[1],
-            emb_dim=256, # 16 by default, augment in the future
-            features_low=pretrain_ds.features_low,
-            features_high=pretrain_ds.features_high,
-            corruption_rate=corruption_rate,
-            dropout=dropout
-        ).to(device)
+            # Build and Load model
+            # build model
+            pretrained_model = SCARF_4layers(
+                input_dim=pretrain_ds.shape[1],
+                emb_dim=256, # 16 by default, augment in the future
+                features_low=pretrain_ds.features_low,
+                features_high=pretrain_ds.features_high,
+                corruption_rate=corruption_rate,
+                dropout=dropout
+            ).to(device)
 
-        pretrained_model.load_state_dict(torch.load("saved_models/scarf_4layers.pt"), strict=False)
-        # isolate encoder part
-        encoder = pretrained_model.encoder
+            pretrained_model.load_state_dict(torch.load(f"saved_models/{args.model_name}.pt"), strict=False)
+            # isolate encoder part
+            encoder = pretrained_model.encoder
 
-        # split training set depending on prop value
-        idx = generate_indices(dataset, prop=prop, val_prop=0.01, test_prop=0.5, rs=42)
+            # split training set depending on prop value
+            idx = generate_indices(dataset, prop=prop, val_prop=0.01, test_prop=0.5, rs=42)
 
-        # get embeddings
-        #loader = DataLoader(dataset[:,1:], batch_size=bs, shuffle=False)
-        #embeddings = dataset_embeddings(encoder, loader, device)
+            # split 
+            embeddings_dataset = CancerDatasetTCGA(dataset[:,1:], dataset[:,0], device)
+            dataloaders = get_dataloaders(embeddings_dataset, idx, [bs, bs, bs])
 
-        # split 
-        embeddings_dataset = CancerDatasetTCGA(dataset[:,1:], dataset[:,0], device)
-        dataloaders = get_dataloaders(embeddings_dataset, idx, [bs, bs, bs])
-
-        # build MLP
-        config_nn = {
-            "epochs":100,
-            "lr_init":1e-4,
-            "early_stop":30,
-            "optim":optim.Adam,
-            "bn":True,
-            "dropout_rate":0.0
-        }
+            # build MLP
+            config_nn = {
+                "epochs":args.epoch,
+                "lr_init":1e-4,
+                "early_stop":30,
+                "optim":optim.Adam,
+                "bn":True,
+                "dropout_rate":0.0
+            }
         
-        head = LastLayer(input_dim=256, output_dim=nb_classes).to(device)
+            head = LastLayer(input_dim=256, output_dim=nb_classes).to(device)
         
-        # Merge encoder and MLP
-        model = nn.Sequential(encoder, head)
-        print(model)
+            # Merge encoder and MLP
+            model = nn.Sequential(encoder, head)
         
-        train_nn(config_nn, dataloaders, model, weights=None, early_stop=config_nn["early_stop"], log=False, logger=logger, test=True, mlp_only=True)
-        logger.next_run()
-        logger.show_progression()
+            train_nn(config_nn, dataloaders, model, weights=None, early_stop=config_nn["early_stop"], log=False, logger=logger, test=True, mlp_only=True)
+            logger.next_run()
+            logger.show_progression()
 
-    logger.save_csv()
+        logger.save_csv()
+        
+if __name__ == "__main__" :
+    main()
 
--- scripts/scarf/pretraining.py
+++ scripts/scarf/pretraining.py
@@ -22,95 +22,152 @@ from ssrl_rnaseq.scarf.dataset import ExampleDataset
 from ssrl_rnaseq.data import *
 from ssrl_rnaseq.training_utils import *
 
+import argparse
+
 #################
 
 
-# Load data
-x_unlabel = read_process_data_TCGA_unlabel('../data/TCGA/pretrain_data.parquet')
-#x_unlabel = read_process_data_TCGA_unlabel('../data/TCGA/100Best_pretrain_data.parquet.gzip')
-#x_unlabel = x_unlabel[:,1:]
-
-
-# Training
-corruption_rate = 0.3
-dropout = 0.1
-batch_size = 256 # default 256
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(device)
-
-# Get validation set
-x_unlabel, x_unlabel_valid = train_test_split(x_unlabel, test_size=0.1, random_state=42)
-
-train_loader = DataLoader(x_unlabel, batch_size=batch_size, shuffle=True)
-valid_loader = DataLoader(x_unlabel_valid, batch_size=batch_size, shuffle=True)
-
-# build model
-"""
-# 1st architecture
-encoder = SCARF_baseline(
-    input_dim=pretrain_ds.shape[1],
-    emb_dim=100, # 16 by default, augment in the future
-    features_low=x_unlabel.min(axis=0),
-    features_high=x_unlabel.max(axis=0),
-    corruption_rate=corruption_rate,
-    dropout=dropout
-).to(device)"""
-
-# 2nd architeture
-model = SCARF_4layers(
-    input_dim=x_unlabel.shape[1],
-    emb_dim=256, # 16 by default, augment in the future
-    features_low=x_unlabel.min(axis=0),
-    features_high=x_unlabel.max(axis=0),
-    corruption_rate=corruption_rate,
-    dropout=dropout
-).to(device)
-"""
-# 3rd architecture
-model = SCARF_SubTab(
-    input_dim=x_unlabel.shape[1],
-    emb_dim=784, # 16 by default, augment in the future
-    features_low=x_unlabel.min(axis=0),
-    features_high=x_unlabel.max(axis=0),
-    corruption_rate=corruption_rate,
-    dropout=dropout
-).to(device)
-"""
-
-import time
-#epochs = 2000
-epochs = 1000
-
-optimizer = Adam(model.parameters(), lr = 1e-4)
-ntxent_loss = NTXent()
-
-
-loss_history = []
-valid_loss_history = []
-train_loss_history = []
-
-start = time.time()
-for epoch in range(1, epochs+1) :
-    valid_loss = valid_loss_f(model, ntxent_loss, valid_loader, device)
-    valid_loss_history.append(valid_loss)
-    train_loss = valid_loss_f(model, ntxent_loss, train_loader, device)
-    train_loss_history.append(train_loss)
-    epoch_loss = train_epoch(model, ntxent_loss, train_loader, optimizer, device)
-    loss_history.append(epoch_loss)
+def main():
+    parser = argparse.ArgumentParser()
     
-    if epoch % 10 == 0 :
-        print(f'Epoch {epoch}/{epochs} : loss = {loss_history[-1]}')
-        # save model as .pt
-        torch.save(model.state_dict(), 'saved_models/scarf_4layers_test.pt')
+    # Required parameters
+    parser.add_argument(
+        "--data_file",
+        default=None,
+        type=str,
+        required=True,
+        help="The pretraining data file. Must be in .parquet format."
+    )
+    parser.add_argument(
+        "--batch_size",
+        default=256,
+        type=int,
+        help="batch size"
+    )
+    parser.add_argument(
+        "--epoch",
+        default=1000,
+        type=int,
+        help="number of epoch"
+    )
+    parser.add_argument(
+        "--dropout",
+        default=0.1,
+        type=float,
+        help="dropout rate"
+    )
+    parser.add_argument(
+        "--model_name",
+        default="scarf",
+        type=str,
+        help="name given to the pretrained model"
+    )
+    parser.add_argument(
+        "--history_name",
+        default="scarf.",
+        type=str,
+        help="name of the loss history file"
+    )
+    parser.add_argument(
+        "--corruption_rate",
+        default=0.3,
+        type=float,
+        help="corruption rate"
+    )
+    
+    args = parser.parse_args()
+    
 
-end = time.time()
-print(end-start)
+    # Load data
+    x_unlabel = read_process_data_TCGA_unlabel(args.data_file)
+    #x_unlabel = read_process_data_TCGA_unlabel('../data/TCGA/100Best_pretrain_data.parquet.gzip')
+    #x_unlabel = x_unlabel[:,1:]
 
 
-# save history
-df = pd.DataFrame(columns=['epoch', 'loss', 'valid_loss', 'train_loss'])
-df['epoch'] = [i for i in range(epochs)]
-df['loss'] = loss_history
-df['valid_loss'] = valid_loss_history
-df['train_loss'] = train_loss_history
-df.to_csv('training_csv/scarf_4layers_test.csv')
+    # Training
+    corruption_rate = args.corruption_rate
+    dropout = args.dropout
+    batch_size = args.batch_size # default 256
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(device)
+
+    # Get validation set
+    x_unlabel, x_unlabel_valid = train_test_split(x_unlabel, test_size=0.1, random_state=42)
+
+    train_loader = DataLoader(x_unlabel, batch_size=batch_size, shuffle=True)
+    valid_loader = DataLoader(x_unlabel_valid, batch_size=batch_size, shuffle=True)
+
+    # build model
+    """
+    # 1st architecture
+    encoder = SCARF_baseline(
+        input_dim=pretrain_ds.shape[1],
+        emb_dim=100, # 16 by default, augment in the future
+        features_low=x_unlabel.min(axis=0),
+        features_high=x_unlabel.max(axis=0),
+        corruption_rate=corruption_rate,
+        dropout=dropout
+    ).to(device)"""
+
+    # 2nd architeture
+    model = SCARF_4layers(
+        input_dim=x_unlabel.shape[1],
+        emb_dim=256, # 16 by default, augment in the future
+        features_low=x_unlabel.min(axis=0),
+        features_high=x_unlabel.max(axis=0),
+        corruption_rate=corruption_rate,
+        dropout=dropout
+    ).to(device)
+    """
+    # 3rd architecture
+    model = SCARF_SubTab(
+        input_dim=x_unlabel.shape[1],
+        emb_dim=784, # 16 by default, augment in the future
+        features_low=x_unlabel.min(axis=0),
+        features_high=x_unlabel.max(axis=0),
+        corruption_rate=corruption_rate,
+        dropout=dropout
+    ).to(device)
+    """
+
+    import time
+    #epochs = 2000
+    epochs = args.epoch
+
+    optimizer = Adam(model.parameters(), lr = 1e-4)
+    ntxent_loss = NTXent()
+
+
+    loss_history = []
+    valid_loss_history = []
+    train_loss_history = []
+
+    start = time.time()
+    for epoch in range(1, epochs+1) :
+        valid_loss = valid_loss_f(model, ntxent_loss, valid_loader, device)
+        valid_loss_history.append(valid_loss)
+        train_loss = valid_loss_f(model, ntxent_loss, train_loader, device)
+        train_loss_history.append(train_loss)
+        epoch_loss = train_epoch(model, ntxent_loss, train_loader, optimizer, device)
+        loss_history.append(epoch_loss)
+    
+        if epoch % 10 == 0 :
+            print(f'Epoch {epoch}/{epochs} : loss = {loss_history[-1]}')
+            # save model as .pt
+            torch.save(model.state_dict(), f'saved_models/{args.model_name}.pt')
+
+    end = time.time()
+    print(end-start)
+
+
+    # save history
+    df = pd.DataFrame(columns=['epoch', 'loss', 'valid_loss', 'train_loss'])
+    df['epoch'] = [i for i in range(epochs)]
+    df['loss'] = loss_history
+    df['valid_loss'] = valid_loss_history
+    df['train_loss'] = train_loss_history
+    df.to_csv(f'training_csv/{args.history_name}.csv')
+    
+    
+if __name__ == '__main__' :
+    main()
